3ded73a41f5fc8459833630fb4efb362bc8f21b5
AbhiraajV <abhiraajverma@gmail.com>
2024-05-31 10:38:55 +0530

    basic working

--- server.ts
+++ server.ts
@@ -1,14 +1,32 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser";
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/identify", async (req, res) => {
   try {
-    const { email, phonenumber } = req.body;
-    // const posts = await prisma.post.findMany();
-    res.json({ email, phonenumber });
+    const { email = null, phoneNumber = null } = req.body;
+    const contacts = await prisma.contact.findMany({
+      where: {
+        OR: [{ email }, { phoneNumber }],
+      },
+    });
+
+    if (contacts.length === 0) {
+      const createContact = await prisma.contact.create({
+        data: {
+          email,
+          phoneNumber,
+          linkPrecedence: "PRIMARY",
+        },
+      });
+      return res.json({ contacts: [createContact], request: req.body });
+    }
+    return res.json({ email, phoneNumber });
   } catch (error) {
     console.error("Error fetching posts:", error);
     res.status(500).json({ message: "Error fetching posts" });
